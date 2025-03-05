9f8418b46e81aeaa8662d05d4aef48232d3e7c2e
stefpi <stef@ceatanostra.org>
2025-03-04 19:54:49 -0500

    change leadership image

---
+++ app/components/ProjectPDF/ProjectPDF.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useGlobalContext } from "@/Context/store";
+
+export default function ProjectPDF() {
+    const {
+        isEditing,
+        projectPDFUrl,
+        setProjectPDFUrl,
+        errors,
+    } = useGlobalContext();
+
+    return (
+        <>
+        </>
+    )
+}
