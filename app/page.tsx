781c4aa9e3ce0bd029686325839a51ae20c9beaa
AkodingZ <ak.codingstudent@gmail.com>
2025-09-30 13:17:17 -0400

    fixed issue 5,6 and 7. Where 7's new discord link is on community discord only for now.

--- app/page.tsx
+++ app/page.tsx
@@ -584,7 +584,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-center relative !overflow-hidden">
           <Title
             title="Our National Conference"
-            subtitle="CUCAI 2023 had 320+ attendees..."
+            subtitle="CUCAI 2025 had 340+ attendees..."
           ></Title>
         </div>
         <div className="h-[240px] md:h-[290px] lg:h-[400px] 2xl:h-[437px] w-[auto]">
@@ -604,7 +604,7 @@ export default function Home() {
           <BtnLink
             isExternalLink={true}
             text="PROCEEDINGS"
-            href="https://qmind.ca/CUCAI_Proceedings_2023.pdf"
+            href="https://cucai.ca/2025_proceedings.pdf"
             className={`${tradeGothic.className} `}
           />
         </div>
@@ -615,7 +615,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-center relative !overflow-hidden">
           <Title
             title="Our Product Incubator"
-            subtitle="InQUbate Was partnered with AWS Activate [‘22-’23]"
+            subtitle="InQUbate was partnered with AWS Activate [‘22-’23]"
           ></Title>
         </div>
         <div className="h-[240px] md:h-[290px] lg:h-[400px] 2xl:h-[437px] w-[auto]">
